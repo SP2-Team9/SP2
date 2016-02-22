b162afd270ef8c8e0f098d40e8593d18e31889ed
plsee <bbplsee1@gmail.com>
2016-02-22 10:59:03 +0800

    Reason: Added Bullet cpp, Bullet H
    Checked by Darrus

---
+++ SP-2 Framework/CGDarrus/Source/Bullet.cpp
@@ -0,0 +1,129 @@
+/////////////////////////////////////////////////////////////////
+/*!
+
+* \File Name: Vehicles.cpp
+
+* \author: Wong Keng Han Ashley
+
+* \date: 22 feb 2016
+
+* \description: functions and data for all bullets
+
+*/
+/////////////////////////////////////////////////////////////////
+
+#include "Bullet.h"
+
+
+/////////////////////////////////////////////////////////////////
+/*!
+
+* \method: constructor
+
+* \author: Wong Keng Han Ashley
+
+* \date: 22 feb 2016
+
+* \description: constructor for Bullets
+
+*/
+/////////////////////////////////////////////////////////////////
+Bullet::Bullet(Vector3 newView, Vector3 playerPos) :
+bulletSpeed(1000)
+{
+
+    Pos = playerPos;
+    View = newView;
+    View.Normalize();
+    initialPosition = playerPos;
+
+}
+
+/////////////////////////////////////////////////////////////////
+/*!
+
+* \method: Destructor
+
+* \author: Wong Keng Han Ashley
+
+* \date: 22 feb 2016
+
+* \description: Destructor for Bullets
+
+*/
+/////////////////////////////////////////////////////////////////
+Bullet::~Bullet(){
+
+
+
+
+
+}
+
+/////////////////////////////////////////////////////////////////
+/*!
+
+* \method: bulletUpdate
+
+* \author: Wong Keng Han Ashley
+
+* \date: 22 feb 2016
+
+* \description: update Bullets
+
+*/
+/////////////////////////////////////////////////////////////////
+void Bullet::bulletUpdate(double dt){
+
+    bulletMovement(dt);
+
+}
+
+/////////////////////////////////////////////////////////////////
+/*!
+
+* \method: bulletMovement
+
+* \author: Wong Keng Han Ashley
+
+* \date: 22 feb 2016
+
+* \description: changes the position of the bullet
+
+*/
+/////////////////////////////////////////////////////////////////
+void Bullet::bulletMovement(double dt){
+
+
+    Pos += View * bulletSpeed * dt;
+
+
+}
+
+
+/////////////////////////////////////////////////////////////////
+/*!
+
+* \method: bulletMovement
+
+* \author: Wong Keng Han Ashley
+
+* \date: 22 feb 2016
+
+* \description: changes the position of the bullet
+
+*/
+/////////////////////////////////////////////////////////////////
+bool Bullet::furtherThanBulletMaxRange(){
+
+
+    if (initialPosition.distanceBetween2points(Pos) > bulletMaxRange){
+
+        return true;
+
+    }
+
+
+    return false;
+
+}
